1900007f6c569912dd3daa34cde88a314382bb21
Rui Han <hrcn1128@gmail.com>
2021-03-22 14:07:19 -0400

    redux and router init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+// pages
+import Home from './pages/Home'
+import MovieList from './pages/MovieList'
+import Like from './pages/Like'
+import Block from './pages/Block'
+
+// components
+import Nav from './components/Nav'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Nav />
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/movielist" exact component={MovieList} />
+          <Route path="/like" exact component={Like} />
+          <Route path="/block" exact component={Block} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux'
+
+// import all the reducers
+
+const allReducers = combineReducers({
+
+})
+
+export default allReducers
