52ccba74481d228f7161571747979d0917c11a9a
Rui Han <hrcn1128@gmail.com>
2021-03-23 15:19:33 -0400

    added Navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,35 @@
 import React from 'react'
-
 import { Link } from 'react-router-dom'
+// styling
+import { AppBar, Toolbar, Button } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(2)
+  }
+}))
 
 function Nav() {
+  const classes = useStyles()
+
   return (
-    <div>
-      Navbar
-    </div>
+    <AppBar position="static">
+      <Toolbar>
+        <Button className={classes.button} component={Link} to="/" color="inherit" size="small">
+          Home
+        </Button>
+        <Button className={classes.button} component={Link} to="/movielist" color="inherit" size="small">
+          Movie List
+        </Button>
+        <Button className={classes.button} component={Link} to="/like" color="inherit" size="small">
+          Liked Movies
+        </Button>
+        <Button className={classes.button} component={Link} to="/block" color="inherit" size="small">
+          Blocked Movies
+        </Button>
+      </Toolbar>
+    </AppBar>
   )
 }
 
