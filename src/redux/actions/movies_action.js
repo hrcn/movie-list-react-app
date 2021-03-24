283196a3aa722649314f5eac93f715b2394f6af5
Kisenlee <32342736+Kisenlee@users.noreply.github.com>
2021-03-24 16:09:52 -0700

    redux

---
+++ src/redux/actions/movies_action.js
@@ -0,0 +1,22 @@
+import { FETCH_API } from 'middleware/api';
+import * as actions from 'constants/actionTypes';
+
+export const getMovie = (movieId) => ({
+  [FETCH_API]: {
+    params: {  },
+    endpoint: `/movie/${movieId}`,
+    method: 'GET',
+    types: [
+      actions.GET_MOVIE_REQUEST,
+      {
+        type: actions.GET_MOVIE_SUCCESS,
+        payload: (action, state, res) => {
+          return res.json().then(json => {
+            return json;
+          });
+        }
+      },
+      actions.GET_MOVIE_ERROR
+    ]
+  }
+});
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux'
+import MoviesReducer from './movie_reducer';
 
 // import all the reducers
 
 const allReducers = combineReducers({
-
+    MoviesReducer,
 })
 
 export default allReducers
